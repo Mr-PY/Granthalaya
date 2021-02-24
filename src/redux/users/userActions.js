e3650bb639b18f7175c4231cf2b7d93d80301468
MrPY <pranayprasad84@gmail.com>
2021-02-24 16:33:35 +0530

    Add meta for SEO

--- src/redux/users/userActions.js
+++ src/redux/users/userActions.js
@@ -1,63 +1,65 @@
-import { setSnackbar } from '../../redux'
+import { setSnackbar } from "../../redux";
 
+export const AssignBookToUser = () => {};
 
-export const AssignBookToUser = () => {
-    
-}
-
-export const modifyuser = () => {
-
-}
+export const modifyuser = () => {};
 
 export const modifyUserProfile = (data) => {
-    return(dispatch, getState, {getFirebase, getFirestore}) =>{
-        //firebase Stuff
-        const db = getFirestore();
-        const userRef = db.collection('users').doc(data.id);
-        userRef.update({
-            user_name: data.user_name,
-            user_phone: data.user_phone,
-            user_image: data.user_image
-        }
-        ).then(()=>{
-            dispatch(
-                setSnackbar(true, 'success', "Profile Updated Successfully")
-            )
-            data.setEditDetailsOpen(false)
-        }).catch((error)=>{
-            dispatch(
-                setSnackbar(true, 'error', `Error occured: ${error.message}`)
-            )
-        })
-    }
-}
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
+    //firebase Stuff
+    const db = getFirestore();
+    const userRef = db.collection("users").doc(data.id);
+    userRef
+      .update({
+        user_name: data.user_name,
+        user_phone: data.user_phone,
+        user_image: data.user_image,
+      })
+      .then(() => {
+        dispatch(setSnackbar(true, "success", "Profile Updated Successfully"));
+        data.setEditDetailsOpen(false);
+      })
+      .catch((error) => {
+        dispatch(setSnackbar(true, "error", `Error occured: ${error.message}`));
+      });
+  };
+};
 
 export const updatePassword = (data) => {
-    return(dispatch, getState, {getFirebase, getFirestore}) =>{
-        //firebase Stuff
-        const firebase = getFirebase();
-        const user = firebase.auth().currentUser;
-        const credential = firebase.auth.EmailAuthProvider.credential(
-                            user.email, data.oldPassword);
-        user.reauthenticateWithCredential(credential)
-            .then(()=>{
-                user.updatePassword(data.newPassword).then(() =>{
-                dispatch(
-                    setSnackbar(true, 'success', "Password Updated Successfully")
-                )
-                data.setLoading(false)
-                data.setChangePasswordOpen(false)
-            }).catch((error)=>{
-                dispatch(
-                    setSnackbar(true, 'error', `Unable to update password: ${error.message}`)
-                    )
-                });
-                data.setLoading(false)
-        }).catch((error)=>{
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
+    //firebase Stuff
+    const firebase = getFirebase();
+    const user = firebase.auth().currentUser;
+    const credential = firebase.auth.EmailAuthProvider.credential(
+      user.email,
+      data.oldPassword
+    );
+    user
+      .reauthenticateWithCredential(credential)
+      .then(() => {
+        user
+          .updatePassword(data.newPassword)
+          .then(() => {
             dispatch(
-                setSnackbar(true, 'error', 'Invalid Password')
-            )
-            data.setLoading(false)
-        })
-    }
-}
+              setSnackbar(true, "success", "Password Updated Successfully")
+            );
+            data.setLoading(false);
+            data.setChangePasswordOpen(false);
+          })
+          .catch((error) => {
+            dispatch(
+              setSnackbar(
+                true,
+                "error",
+                `Unable to update password: ${error.message}`
+              )
+            );
+          });
+        data.setLoading(false);
+      })
+      .catch((error) => {
+        dispatch(setSnackbar(true, "error", "Invalid Password"));
+        data.setLoading(false);
+      });
+  };
+};
